f28281aab6c9c1bae4ad7ac0e0de2876d0ec9c82
Atique Ahmed <atiqueahmed@Atiques-MacBook-Pro.local>
2023-09-25 12:39:58 +0530

    change in the endpoint url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   produrl: 'https://4bjxyf7z29.execute-api.ap-south-1.amazonaws.com/production',
   localurl: 'http://localhost:8081',
-  url : 'http://localhost:8081'
+  url : 'https://4bjxyf7z29.execute-api.ap-south-1.amazonaws.com/production'
 };
